87e472a897fe5624f9e4ee9e84abca44851b35a7
Thomas Pope <tpope0928@gmail.com>
2018-09-04 22:25:03 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,9 +1,8 @@
 class Author
-
   attr_accessor :name
-  
+
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @@all << self
@@ -12,23 +11,21 @@ class Author
   def self.all
     @@all
   end
-  
+
   def add_post(post)
     post.author = self
   end
-  
-  def add_post_by_title
-    post = post.new(title)
-    post.artist = self
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
   end
-  
+
   def posts
-    post.all.select {|post| post.artist == self}
+    Post.all.select {|post| post.author == self}
   end
 
   def self.post_count
-    post.all.count
+    Post.all.count
   end
-
-
 end
