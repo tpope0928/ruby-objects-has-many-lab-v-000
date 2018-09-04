2700167093374f7b329617605a236079f7e07e34
Thomas Pope <tpope0928@gmail.com>
2018-09-04 22:27:21 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,10 @@
-class post
-  attr_accessor :ti, :author
+class Post
+  attr_accessor :title, :author
 
   @@all = []
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @@all << self
   end
 
